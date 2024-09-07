af2560243574e0aa3dd8f5f3c56eba18d394b2d7
Maksim G <gulin.maksim@icloud.com>
2024-09-07 16:38:17 -0700

    TC_API_0.0.1.1

--- src/test/java/api/tests/userTests.java
+++ src/test/java/api/tests/userTests.java
@@ -1,17 +1,8 @@
 package api.tests;
 
 import api.assertions.Conditions;
-import io.restassured.RestAssured;
-import io.restassured.filter.log.RequestLoggingFilter;
-import io.restassured.filter.log.ResponseLoggingFilter;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
 import api.models.user.User;
-import listeners.CustomAllureRestAssured;
-import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.*;
-import api.services.UserService;
-import utils.AppConfig;
 import utils.RandomTestData;
 
 import java.util.ArrayList;
@@ -19,7 +10,7 @@ import java.util.ArrayList;
 import static io.restassured.RestAssured.given;
 
 
-public class UserTests extends BaseTest {
+public class userTests extends BaseTest {
     private User user;
 
     @BeforeEach
