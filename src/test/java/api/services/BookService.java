9b3bb7ca09086e2bd27e7f9882eeff41c68fa9ad
Maksim G <gulin.maksim@icloud.com>
2024-09-11 23:26:22 -0700

    TC_API_0.0.1.4

--- src/test/java/api/services/BookService.java
+++ src/test/java/api/services/BookService.java
@@ -2,6 +2,7 @@ package api.services;
 
 import api.assertions.AssertableResponse;
 import api.models.book.Book;
+import api.tests.BaseTest;
 import com.github.javafaker.Faker;
 import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
@@ -13,10 +14,10 @@ import static io.restassured.RestAssured.given;
 public class BookService {
     private final Faker faker = new Faker();
 
-    public AssertableResponse addNewBook(RequestSpecification specification, Book book){
-        ValidatableResponse response = given().spec(specification)
+    public AssertableResponse addNewBook(Book book){
+        ValidatableResponse response = given().spec(BaseTest.getSpecification())
                         .contentType(ContentType.MULTIPART)
-                        .multiPart("name", book.getName())  // Передаем имя книги
+                        .multiPart("name", book.getName())
                         .multiPart("description", book.getDescription())
                         .multiPart("description_html", book.getDescriptionHtml())
                         .multiPart("image", RandomTestData.getBookCover(), "image/jpg")
@@ -27,17 +28,32 @@ public class BookService {
         return new AssertableResponse(response);
     }
 
-    public AssertableResponse getAvailableBooksList(RequestSpecification specification) {
-        ValidatableResponse response =  given().spec(specification)
+    public AssertableResponse getAvailableBooksList() {
+        ValidatableResponse response =  given().spec(BaseTest.getSpecification())
                 .get("api/books")
                 .then();
         return new AssertableResponse(response);
     }
 
-    public AssertableResponse getBookById(RequestSpecification specification, Integer id){
-        ValidatableResponse response = given().spec(specification)
+    public AssertableResponse getBookById(Integer id){
+        ValidatableResponse response = given().spec(BaseTest.getSpecification())
                 .get("api/books/"+id)
                 .then();
+        return new AssertableResponse(response);
+    }
+
+    public AssertableResponse updateBookById(Book originalBook) {
+        Book updatedBook = RandomTestData.getRandomBook();
+
+        ValidatableResponse response = given().spec(BaseTest.getSpecification())
+                .contentType(ContentType.MULTIPART)
+                .multiPart("name", updatedBook.getName())
+                .multiPart("description", updatedBook.getDescription())
+                .multiPart("description_html", updatedBook.getDescriptionHtml())
+                .multiPart("image", RandomTestData.getBookCover(), "image/jpg")
+                .put("api/books/" + originalBook.getId())
+                .then();
+
         return new AssertableResponse(response);
     }
 
